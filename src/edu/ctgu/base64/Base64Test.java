33fc71637cf21e6d4034905198d0cd5adaa282f8
AZHELLRGX <776379772@qq.com>
2017-08-11 11:09:55 +0800

    base64one

--- src/edu/ctgu/base64/Base64Test.java
+++ src/edu/ctgu/base64/Base64Test.java
@@ -1,15 +1,19 @@
 package edu.ctgu.base64;
 
+import org.bouncycastle.util.encoders.Base64Encoder;
+
 /**
  * @author AZHELL E-mail:r776379772@Gmail.com
  * @version 创建时间：2017年8月11日 上午10:36:51
  * @ClassDescription 类说明:
  */
 public class Base64Test {
+	private static String s = "I Love Java";
+	
 	public static void main(String[] args) {
 		
 	}
 	public void jdkBase64(){
-		
+		Base64Encoder encoder = new Base64Encoder();
 	}
 }

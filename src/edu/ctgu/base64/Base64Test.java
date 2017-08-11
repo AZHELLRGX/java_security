e9658c7c7b4270379f03020ea45cd2cae3e95400
AZHELLRGX <776379772@qq.com>
2017-08-11 10:40:11 +0800

    创建计算机安全与密码项目

---
+++ src/edu/ctgu/base64/Base64Test.java
@@ -0,0 +1,15 @@
+package edu.ctgu.base64;
+
+/**
+ * @author AZHELL E-mail:r776379772@Gmail.com
+ * @version 创建时间：2017年8月11日 上午10:36:51
+ * @ClassDescription 类说明:
+ */
+public class Base64Test {
+	public static void main(String[] args) {
+		
+	}
+	public void jdkBase64(){
+		
+	}
+}
